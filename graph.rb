ddc202f4b3fd1bee5bb3197bcc5cd81f806585a6
JeongWooYeong <wjd030811@gmail.com>
2020-04-06 17:05:44 +0900

    [ADD] 그래프 변수 네이밍과 주석 수정

--- graph.rb
+++ graph.rb
@@ -5,51 +5,51 @@ class Graph
     @count = 0
   end
 
-  # 노드를 추가하는 메소드. 가변인자로 추가한 노드와 연결되는 간선을 받는다.
-  # 노드는 대문자 A부터 차례대로 할당되며, graph의 key에 노드의 값, value에 노드와 연결되는 다른 노드의 간선과,
+  # 정점을 추가하는 메소드. 가변인자로 추가한 정점의 연결되는 간선을 받는다.
+  # 정점의 값은 대문자 A부터 차례대로 할당되며, graph의 key에 정점의 값, value에 정점에 연결되는 다른 정점의 간선과,
   # 해당 간선의 cost가 각각 key: value 로 연결되는 해시를 할당한다.
-  def add_node(**kwargs)
-    new_node = (65 + @count).chr.to_sym
-    @graph[new_node] = {}
+  def add_vertex(**kwargs)
+    new_vertex = (65 + @count).chr.to_sym
+    @graph[new_vertex] = {}
 
-    kwargs.each do |node, cost|
-      next unless @graph[node]
+    kwargs.each do |vertex, cost|
+      next unless @graph[vertex]
 
-      @graph[new_node][node] = cost
+      @graph[new_vertex][vertex] = cost
     end
 
     @count += 1
-    @graph[new_node]
+    @graph[new_vertex]
   end
 
-  # 간선을 추가하는 메소드, 간선의 방향을 start_node와 end_node인자로 받으며, 간선의 cost도 설정할 수 있다.
-  # 만약 간선에 올바르지 않은 노드가 포함된다면, false를 반환하며 간선이 추가되지 않는다.
-  def add_vertex(start_node, end_node, cost)
-    return false unless @graph[start_node] && @graph[end_node]
+  # 간선을 추가하는 메소드, 간선의 방향을 start_vertex와 end_vertex 인자로 받으며, 간선의 cost도 설정할 수 있다.
+  # 만약 간선에 올바르지 않은 정점이 포함된다면, false를 반환하며 간선이 추가되지 않는다.
+  def add_edge(start_vertex, end_vertex, cost)
+    return false unless @graph[start_vertex] && @graph[end_vertex]
 
-    @graph[start_node][end_node] = cost
-    @graph[start_node]
+    @graph[start_vertex][end_vertex] = cost
+    @graph[start_vertex]
   end
 
-  # 노드를 삭제하는 메소드. 그래프를 순회하며, 삭제할 노드와 연결된 간선을 전부 삭제한 후, 해당 노드를 삭제한다.
-  def remove_node(node)
-    return false unless @graph[node]
+  # 정점을 삭제하는 메소드. 그래프를 순회하며, 삭제할 정점과 연결된 간선을 전부 삭제한 후, 해당 정점을 삭제한다.
+  def remove_vertex(value)
+    return false unless @graph[value]
 
     @graph.each do |key, vertex|
       next if vertex.empty?
 
-      vertex.each_key do |end_node|
-        @graph[key].delete(end_node) if node == end_node
+      vertex.each_key do |end_vertex|
+        @graph[key].delete(end_vertex) if value == end_vertex
       end
     end
 
-    @graph.delete(node)
+    @graph.delete(value)
   end
 
-  # 간선을 삭제하는 메소드, 간선이 시작되는 노드와 끝나는 노드를 인자로 넘겨받아 삭제한다.
-  def remove_vertex(start_node, end_node)
-    if @graph[start_node] && @graph[end_node]
-      @graph[start_node].delete(end_node)
+  # 간선을 삭제하는 메소드, 간선이 시작되는 정점과 끝나는 정점을 인자로 넘겨받아 삭제한다.
+  def remove_edge(start_vertex, end_vertex)
+    if @graph[start_vertex] && @graph[end_vertex]
+      @graph[start_vertex].delete(end_vertex)
     else
       false
     end
@@ -63,17 +63,17 @@ end
 
 graph = Graph.new
 
-graph.add_node
-graph.add_node
-graph.add_node
+graph.add_vertex
+graph.add_vertex
+graph.add_vertex
 
 graph.show_graph
 
-graph.add_node(B: 1, C: 2)
-graph.add_vertex(:B, :C, 3)
+graph.add_vertex(B: 1, C: 2)
+graph.add_edge(:B, :C, 3)
 
 graph.show_graph
 
-graph.remove_node(:B)
-graph.remove_vertex(:D, :C)
+graph.remove_vertex(:B)
+graph.remove_edge(:D, :C)
 graph.show_graph
